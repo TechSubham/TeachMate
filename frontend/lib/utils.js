53e2a72b144018b5d6274f1a9d3fb15837417ff1
TechSubham <subhamchauhan523@gmail.com>
2025-11-03 19:29:32 +0530

    issue fix

--- frontend/lib/utils.js
+++ frontend/lib/utils.js
@@ -5,4 +5,4 @@ export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
 
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://teachmate-bc-1.onrender.com';
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://teachmate-bc-2.onrender.com';
